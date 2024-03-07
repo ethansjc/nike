dad5a850db71566999f27d5081d3cc5550076bea
ethansjc <ethansjcheong@gmail.com>
2024-03-07 16:18:39 -0500

    Completed ShoeCard component and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,36 @@ import {
   SpecialOffer,
   CustomerReviews,
   Subscribe,
-  Footer
+  Footer,
 } from "./sections";
 import Nav from "./components/Nav";
 
-
 const App = () => (
   <main className="relative">
-    <Nav/>
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero/>
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
     </section>
     <section className="padding">
-    <PopularProducts/>
+      <PopularProducts />
     </section>
     <section className="padding">
-    <SuperQuality/>
+      <SuperQuality />
     </section>
     <section className="padding padding-x py-10">
-    <Services/>
+      <Services />
     </section>
     <section className="padding">
-    <SpecialOffer/>
+      <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-    <CustomerReviews/>
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-    <Subscribe/>
+      <Subscribe />
     </section>
     <section className="bd-black padding-x padding-t pb-8">
-    <Footer/>
+      <Footer />
     </section>
   </main>
 );
