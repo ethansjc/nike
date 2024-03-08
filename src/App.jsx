45d1540d48a834a60cea92db9d1050abb48fae79
ethansjc <ethansjcheong@gmail.com>
2024-03-08 16:14:13 -0500

    Completed Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => (
     <section className="padding-x sm:py-32 py-16 w-full">
       <Subscribe />
     </section>
-    <section className="bd-black padding-x padding-t pb-8">
+    <section className="bg-black padding-x padding-t pb-8">
       <Footer />
     </section>
   </main>
